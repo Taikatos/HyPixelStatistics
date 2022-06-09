ed977770ca31e30eaa6d7685674428e43639ece9
Andrei DIaconu <diaconu.4ndr3i@gmail.com>
2022-06-10 00:44:01 +0200

    Aggiunto icone Iniziato stats recicle view adapter

--- app/src/main/java/com/pianopianociproviamo/hypixelstatistics/LoginActivity.kt
+++ app/src/main/java/com/pianopianociproviamo/hypixelstatistics/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.pianopianociproviamo.hypixelstatistics
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 
 /*
     *   companion object {
@@ -26,6 +28,11 @@ public class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+        var text_mail = findViewById<EditText>(R.id.form_login_al_mail)
+        var text_password = findViewById<EditText>(R.id.form_login_al_pw)
+
+
+
         val btn_login_c = findViewById<ImageButton>(R.id.btn_login_confirm)
         btn_login_c.setOnClickListener(){
             Intent(this, MainActivity::class.java).also{
@@ -33,6 +40,23 @@ public class LoginActivity : AppCompatActivity() {
                 finish()
             }
         }
+
+        /*
+        val btn_login_c = findViewById<Image>(R.id.btn_login_confirm)
+        btn_login_c.setOnClickListener {
+
+            if( (TextUtils.isEmpty(text_mail.text.toString())) && (TextUtils.isEmpty(text_password.text.toString())) ) {
+                Toast.makeText(this, "I campi MAIL e PASSWORD non possono essere vuoti")
+            }
+
+            else {
+                    Intent(this, MainActivity::class.java)
+                    startActivity(it)
+                    finish()
+            }
+        }
+        * */
+
     }
 }
 
--- app/src/main/java/com/pianopianociproviamo/hypixelstatistics/MainActivity.kt
+++ app/src/main/java/com/pianopianociproviamo/hypixelstatistics/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pianopianociproviamo.hypixelstatistics
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -24,6 +25,7 @@ public class MainActivity : AppCompatActivity() {
             replace(R.id.flFragment, homeFragment)
         }.commit()
 
+        var text_title_layout = findViewById<TextView>(R.id.ma_hd_text)
         val btn_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         btn_navigation.selectedItemId=R.id.ic_home
@@ -31,17 +33,20 @@ public class MainActivity : AppCompatActivity() {
         btn_navigation.setOnItemSelectedListener {
             var fragment:Fragment?=null
             when (it.itemId) {
-                R.id.ic_home -> fragment = HomeFragment()
-                R.id.ic_stats -> fragment = StatsFragment()
-                R.id.ic_search -> fragment = SearchFragment()
-                R.id.ic_chat -> fragment = ChatFragment()
-                R.id.ic_profile -> fragment = ProfileFragment()
+                R.id.ic_stats -> fragment = statsFragment
+                R.id.ic_search -> fragment = searchFragment
+                R.id.ic_home -> fragment = homeFragment
+                R.id.ic_chat -> fragment = chatFragment
+                R.id.ic_profile -> fragment = profileFragment
             }
+
             fragment?.let { supportFragmentManager.beginTransaction().apply {
                 replace(R.id.flFragment, fragment)
             }.commit() }
             true
         }
+
+        //recycleView = findViewById(R.id.rv_stats)
 
     }
 }
