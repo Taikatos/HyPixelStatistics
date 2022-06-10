c16a9829cafedd6bb96d61da53c43c2395c494c4
Andrei DIaconu <diaconu.4ndr3i@gmail.com>
2022-06-10 12:45:30 +0200

    Gradel sync new implementations

--- app/src/main/java/com/pianopianociproviamo/hypixelstatistics/statsAdapter.kt
+++ app/src/main/java/com/pianopianociproviamo/hypixelstatistics/statsAdapter.kt
@@ -1,8 +1,36 @@
 package com.pianopianociproviamo.hypixelstatistics
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class statsAdapter (){
-    inner class statsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class statsAdapter (val listStats:List<statsViewModel>, var context:Context) : RecyclerView.Adapter<statsAdapter.ViewHolder>() {
+
+    private val title: arrayOf("")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_stats, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val statsViewModel = listStats[position]
+        holder.imgView.setImageResource(R.drawable.ic_arena)
+    }
+
+    override fun getItemCount(): Int {
+        return listStats.size
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imgView: ImageView = itemView.findViewById(R.id.rc_iw_icon)
+        val textView: TextView = itemView.findViewById(R.id.rc_tv_title)
+    }
+
 }
